bca6d133a52bdd0ae4a9baf3b3980f34c74686a5
Daniel Gimelfarb <danielsin12@gmail.com>
2024-02-10 21:38:17 +0200

    dsa

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
   res.send('Hello, Node.js!');
 });
 
-// Start the server and listen on the specified port
+// Start the server and listen on the specified port1
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
